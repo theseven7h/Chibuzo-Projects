bae66cb22e7a364b1373f59fd56578948e27a5e3
theseven7h <jamestauri123@gmail.com>
2025-05-05 12:49:25 +0100

    First Arrays commit

---
+++ NumberArray.java
@@ -0,0 +1,17 @@
+public class NumberArray {
+	public static void main(String[] args) {
+		int[] numbers = new int[5];
+		numbers [0] = 22; numbers[1] = 51; numbers[2] = 38; numbers[3] = 23; numbers[4] = 48; 
+		for (int count = 0; count < 5; count++) {
+			System.out.print(numbers[count] + " ");
+		}
+
+		System.out.println();
+		System.out.println();	
+
+		for (int count = 0; count < 5; count++) {
+			System.out.println(numbers[count]);
+		}
+	}
+}
+	
